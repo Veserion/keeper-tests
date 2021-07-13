e8225c105f13ad5d920ced1362e9581de3b35c24
Daniil <lobanovdanil97@gmail.com>
2021-07-14 01:09:31 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button, notification} from 'antd';
 import './App.css';
+import {transactions} from "./transactions/transactions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const auth = () => (window as any).WavesKeeper.publicState().then((resp: any) => notification.open({message: `Keeper version ${resp.version}`})).catch((e: any) => notification.error({message: 'Keeper is not installed'}))
+    const sign = (tx: any) => (window as any).WavesKeeper.signAndPublishTransaction(tx).then((tx: any) => {
+        notification.open({message: 'Sign tx', description: tx});
+    }).catch((error: any) => {
+        notification.error({message: 'Error!!!', description: JSON.stringify(error, null, 4)});
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button type="primary" onClick={auth} style={{width: '150px', marginBottom: '20px'}}>Check
+                    Keeper</Button>
+                {transactions.map((tx, i) =>
+                    <Button type="primary" onClick={() => sign(tx)} key={i} style={{width: '150px', marginBottom: '20px'}}>
+                        {`Sign ${tx.type} type`}
+                    </Button>)}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/transactions/transactions.ts
@@ -0,0 +1,26 @@
+export const transactions = [
+    {
+        type: 17,
+        data: {
+            name: "New name",
+            description: "blabla",
+            assetId: "doe1e6PU3xp11tQJAiw1fZ9LnmmK2FPH5cKsUsGYm3B",
+            fee: {
+                assetId: "WAVES",
+                tokens: "0.005"
+            }
+        }
+    },
+    {
+        type: 17,
+        data: {
+            name: "New name",
+            description: "blabla",
+            assetId: "doe1e6PU3xp11tQJAiw1fZ9LnmmK2FPH5cKsUsGYm3B",
+            fee: {
+                assetId: "WAVES",
+                tokens: "0.005"
+            }
+        }
+    }
+]
